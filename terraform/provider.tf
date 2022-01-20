053fe1c311f9882a0ab60c225ee0f18f2cd0735b
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-20 19:47:22 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,7 +2,6 @@
 # provider.tf
 
 provider "aws" {
-  version = "~> 3.5.0"
-  profile = "default"
-  region = var.aws_region
+  profile                 = "default"
+  region                  = var.aws_region
 }
