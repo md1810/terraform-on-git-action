0f8bb21c7bf7f503b477d88305c3d2f02937f677
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-20 18:19:50 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,7 @@
 # Specify the provider and access details
 provider "aws" {
   #shared_credentials_file = "$HOME/.aws/credentials"
-  profile                 = "default"
-  region                  = var.aws_region
+  profile = "default"
+  version = "~> 3.0"
+  region  = var.aws_region
 }
