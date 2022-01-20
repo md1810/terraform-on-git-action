b85294c1792483b0bebfd1ed89154ee863671afe
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-20 19:07:49 +0530

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,21 +1,6 @@
 
 # provider.tf
 
-
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "3.5.0"
-    }
-  }
-
-provider "aws" {
-  profile = "default"
-  region = var.aws_region
-    }
-}
-
 provider "aws" {
   source = "hashicorp/aws"
   version = "~> 3.5.0"
