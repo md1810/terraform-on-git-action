84cce45c65fd8b1a8bc343228d7e3dbb649da4c1
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-21 18:27:40 +0530

    Delete destroy.sh

--- destroy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh -l
-git clone https://github.com/md1810/terraform-on-git-action.git
-cd terraform-on-git-action/terraform
-terraform init && terraform state replace-provider registry.terraform.io/-/aws registry.terraform.io/hashicorp/aws -auto-approve
-terraform init && terraform destroy -auto-approve
