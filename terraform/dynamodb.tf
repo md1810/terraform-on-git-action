5d8b225877766616632dd86eb851280a84a0c77b
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-26 16:54:37 +0530

    Update dynamodb.tf

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,13 +1,13 @@
-# resource "aws_dynamodb_table" "terraform-lock" {
-#     name           = "terraform-locks"
-#     read_capacity  = 5
-#     write_capacity = 5
-#     hash_key       = "LockId"
-#     attribute {
-#         name = "LockId"
-#         type = "S"
-#     }
-#     tags = {
-#         "Name" = "DynamoDB Terraform State Lock Table"
-#     }
-# }
+resource "aws_dynamodb_table" "terraform-lock" {
+    name           = "terraform-locks"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockId"
+    attribute {
+        name = "LockId"
+        type = "S"
+    }
+    tags = {
+        "Name" = "DynamoDB Terraform State Lock Table"
+    }
+}
