7e17056b4d070d3526d36f87ea54570c9e4ae97a
Mayur Duduka <71468349+md1810@users.noreply.github.com>
2022-01-26 17:50:27 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform{
     backend "s3" {
         bucket         = "github-terraform-statefile"
         key            = "some_environment/terraform.tfstate"
-        dynamodb_table         = "duve-locks"
+        dynamodb_table         = "terraform-locks"
         encrypt                = true
         skip_region_validation = true
     }
